5d5a3c42ed71c1efff43876fef773f9cfcb15286
bl815v <jebedoyal@udistrital.edu.co>
2025-09-01 11:34:52 -0500

    Mockup 0.0.3: Python integration

---
+++ main.py
@@ -0,0 +1,45 @@
+"""This module initializes and configures the FastAPI application.
+
+It mounts a static files directory to serve HTML, CSS, and JavaScript
+resources and defines the root endpoint that returns the main
+`index.html` file of the project.
+
+Author: Juan Esteban Bedoya <jebedoyal@udistrital.edu.co>
+
+This file is part of ComputerScience2 project.
+
+ComputerScience2 is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as 
+published by the Free Software Foundation, either version 3 of 
+the License, or (at your option) any later version.
+
+ComputerScience2 is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with ComputerScience2. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import os
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def read_root() -> FileResponse:
+    """Returns the main index.html file.
+
+    This endpoint serves the initial HTML page of the application,
+    located inside the `static` directory.
+
+    Returns:
+        FileResponse: The `index.html` file from the static folder.
+    """
+    return FileResponse(os.path.join("static", "index.html"))
